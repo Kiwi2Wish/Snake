3734d4a0f3a90c29d4695f22a1616755a07e6076
Kiwi2Wish <mathis.figoureux@isen.yncrea.fr>
2025-04-28 11:34:47 +0200

    Update app.py
    menu pause + game over

--- app.py
+++ app.py
@@ -1,69 +1,77 @@
-# importing libraries
+
 import pygame
 import time
 import random
 import sys
 
 paused = False
+gameover = False
 
-# Window size
+
 window_x = 720
 window_y = 480
 
-# defining colors
+
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
 
-# Initialising pygame
+
 pygame.init()
 
-# Initialise game window
-pygame.display.set_caption('GeeksforGeeks Snakes')
+
+pygame.display.set_caption('Snakes')
 game_window = pygame.display.set_mode((window_x, window_y))
 
-# FPS (frames per second) controller
+
 fps = pygame.time.Clock()
 
 
-# displaying Score function
 def show_score(choice, color, font, size):
-    # creating font object score_font
     score_font = pygame.font.SysFont(font, size)
-
-    # create the display surface object
-    # score_surface
     score_surface = score_font.render('Score : ' + str(score), True, color)
-
-    # create a rectangular object for the text
-    # surface object
     score_rect = score_surface.get_rect()
-
-    # displaying text
     game_window.blit(score_surface, score_rect)
 
+def menu():
+    global paused, gameover
 
-def menu_pause():
     my_font = pygame.font.SysFont('times new roman', 50)
     small_font = pygame.font.SysFont('times new roman', 30)
 
-    pause_surface = my_font.render('Paused', True, red)
-    pause_rect = pause_surface.get_rect(center=(window_x / 2, window_y / 4))
+    if not gameover:
+        menu_surface = my_font.render('Paused', True, red)
+
+        resume_surface = small_font.render('Press ESC to resume', True, white)
+        resume_rect = resume_surface.get_rect(center=(window_x / 2, window_y / 3+25))
+    else:
+        menu_surface = my_font.render('GAME OVER', True, red)
+
+    menu_rect = menu_surface.get_rect(center=(window_x / 2, window_y / 8))
+
+    score_surface = small_font.render('Score : ' + str(score), True, green)
+    score_rect = score_surface.get_rect(center=(window_x / 2, window_y / 4))
 
     restart_surface = small_font.render('Press R to Restart', True, white)
-    restart_rect = restart_surface.get_rect(center=(window_x / 2, window_y / 2))
+    restart_rect = restart_surface.get_rect(center=(window_x / 2, window_y / 3+75))
 
     quit_surface = small_font.render('Press Q to Quit', True, white)
-    quit_rect = quit_surface.get_rect(center=(window_x / 2, window_y / 2 + 50))
+    quit_rect = quit_surface.get_rect(center=(window_x / 2, window_y / 3 + 125))
 
     while True:
+
+
         game_window.fill(black)
-        game_window.blit(pause_surface, pause_rect)
+        if not gameover:
+            game_window.blit(resume_surface, resume_rect)
+        game_window.blit(menu_surface, menu_rect)
+        game_window.blit(score_surface, score_rect)
         game_window.blit(restart_surface, restart_rect)
         game_window.blit(quit_surface, quit_rect)
         pygame.display.update()
+
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -73,44 +81,17 @@ def menu_pause():
                     game_loop()
                 elif event.key == pygame.K_q:
                     pygame.quit()
-                    quit()
-
-
-# game over function
-def game_over():
-    # creating font object my_font
-    my_font = pygame.font.SysFont('times new roman', 50)
-
-    # creating a text surface on which text
-    # will be drawn
-    game_over_surface = my_font.render(
-        'Your Score is : ' + str(score), True, red)
-
-    # create a rectangular object for the text
-    # surface object
-    game_over_rect = game_over_surface.get_rect()
-
-    # setting position of the text
-    game_over_rect.midtop = (window_x / 2, window_y / 4)
-
-    # blit will draw the text on screen
-    game_window.blit(game_over_surface, game_over_rect)
-    pygame.display.flip()
-
-    # after 2 seconds we will quit the program
-    time.sleep(2)
-
-    # deactivating pygame library
-    pygame.quit()
-
-    # quit the program
-    quit()
-
+                    sys.exit()
+                elif event.key == pygame.K_ESCAPE:
+                    game_loop()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
 def game_loop():
-    global snake_position, snake_body, fruit_position, fruit_spawn, direction, change_to, score, snake_speed
+    global snake_position, snake_body, fruit_position, fruit_spawn, direction, change_to, score, snake_speed, gameover
 
-    # Réinitialisation des variables à chaque nouveau départ
+    gameover = False
     snake_position = [100, 50]
     snake_body = [[100, 50],
                   [90, 50],
@@ -136,7 +117,10 @@ def game_loop():
                 if event.key == pygame.K_RIGHT:
                     change_to = 'RIGHT'
                 if event.key == pygame.K_ESCAPE:
-                    menu_pause()
+                    menu()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
         if change_to == 'UP' and direction != 'DOWN':
             direction = 'UP'
@@ -167,28 +151,28 @@ def game_loop():
         if not fruit_spawn:
             fruit_position = [random.randrange(1, (window_x // 10)) * 10,
                               random.randrange(1, (window_y // 10)) * 10]
-
         fruit_spawn = True
-        game_window.fill(black)
 
+        game_window.fill(black)
         for pos in snake_body:
             pygame.draw.rect(game_window, green,
                              pygame.Rect(pos[0], pos[1], 10, 10))
+
         pygame.draw.rect(game_window, white, pygame.Rect(
             fruit_position[0], fruit_position[1], 10, 10))
 
-        if snake_position[0] < 0 or snake_position[0] > window_x - 10:
-            game_over()
-        if snake_position[1] < 0 or snake_position[1] > window_y - 10:
-            game_over()
 
+        if (snake_position[0] < 0 or snake_position[0] > window_x - 10 or
+            snake_position[1] < 0 or snake_position[1] > window_y - 10):
+            gameover = True
+            menu()
         for block in snake_body[1:]:
             if snake_position[0] == block[0] and snake_position[1] == block[1]:
-                game_over()
+                gameover = True
+                menu()
 
         show_score(1, white, 'times new roman', 20)
         pygame.display.update()
         fps.tick(snake_speed)
-
 
 game_loop()
